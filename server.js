91bcdbc2ea360d8bdcb8c45814c34fff90d315df
Ajitem <ajitempublic3@gmail.com>
2017-02-09 21:13:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {                          //When a get request
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));   //Pick up ui/index.html and send the contents of that file//
 });
 
+app.get('/article-one', function(req,res) {
+    res.send('Article one requested and wil be displayed here')
+});
+
+app.get('/article-two', function(req,res) {
+    res.send('Article two requested and wil be displayed here')
+});
+
+app.get('/article-three', function(req,res) {
+    res.send('Article three requested and wil be displayed here')
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
