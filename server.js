a88034c66f049fd2679991db9ddc1a96d342f927
Ajitem <ajitempublic3@gmail.com>
2017-02-21 17:09:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-var port = 8080; 
-app.listen(8080, function () {
+var port = 8081; 
+app.listen(8081, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
