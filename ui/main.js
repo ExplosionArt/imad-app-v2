611bf92fa213619e35ae268ef1c62d6b21311aa2
Ajitem <ajitempublic3@gmail.com>
2017-02-23 21:01:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,26 @@
 console.log('Loaded!');
+//Counter Code//
+var button=document.getElementById('counter');
+
+button.onClick=function() {
+    //Make a request to counter endpoint//
+    var request= new XMLHttpRequest();
+    
+    
+    //Capture the response and store it in a variable//
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE) {
+            //Action//
+            if(request.status == 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //Not Done yet//
+    }
+   
+    //Make the request//
+    request.open('GET','http://explosionart.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
