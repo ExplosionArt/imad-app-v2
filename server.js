f8091690f367c104fcaf648c1089f9c8f6c43390
Ajitem <ajitempublic3@gmail.com>
2017-02-23 20:15:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var counter=0;
 app.get('/counter', function(req, res) {
     counter=counter+1;
     res.send(counter.toString());           //The response always has to be in the form of a string//
-} )
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
