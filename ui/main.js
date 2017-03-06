1d023215d47fae0df0501fd3ef39dbc4a2b5ac64
Ajitem <ajitempublic3@gmail.com>
2017-03-06 21:13:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,13 @@
 //Counter Code//
 var button = document.getElementById('counter');
 var counter = 0;
+var span = document.getElementById('count');
 
 button.onClick = function() {
     //Make a request to counter endpoint//
   //  var request= new XMLHttpRequest();
     counter = counter + 1;
-    var span = document.getElementById('count');
+    alert('Inside OnClick');
     span.innerHTML = counter.toString();
     //Capture the response and store it in a variable//
    /* request.onReadyStateChange = function() {
