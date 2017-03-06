79b8d48f96a3a6959ea802b116c44a3ef47603ab
Ajitem <ajitempublic3@gmail.com>
2017-03-06 20:11:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,14 +5,14 @@ var counter=0;
 
 button.onClick = function() {
     //Make a request to counter endpoint//
-    var request= new XMLHttpRequest();
+  //  var request= new XMLHttpRequest();
     counter = counter + 1;
     var span=document.getElementById('count');
     span.innerHTML= counter.toString();
     
     
     //Capture the response and store it in a variable//
-    request.onReadyStateChange = function() {
+   /* request.onReadyStateChange = function() {
         if(request.readyState === XMLHttpRequest.DONE) {
             //Action//
             if(request.status === 200) {
@@ -26,5 +26,5 @@ button.onClick = function() {
    
     //Make the request//
     request.open('GET','http://explosionart.imad.hasura-app.io/counter',true);
-    request.send(null);
+    request.send(null);*/
 };
