528dcd8a69174502f8a9f087298ea4b901551a34
Ajitem <ajitempublic3@gmail.com>
2017-03-06 23:42:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names=[];
 app.get('/submit-name/:name', function (req, res) {
     //Get name from request//
     var name = req.params.name;
