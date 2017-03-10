71d0511f8d95c5ea6e094ecdf3a70c14243fb88d
Ajitem <ajitempublic3@gmail.com>
2017-03-10 12:12:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,8 +42,9 @@ app.get('/ui/madi.png', function (req, res) {
 
 function hash(input, salt) {
     //How do create a hash//
-    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');  //password based key derivation function//
+    //Takes the input--> Appends the salt value(this-is-some-random-string)--> Hashes it 10000 times(password-->hash-->hash2....hash10k)-->produces 512byte string
+    return ['pbkdf','10000',salt,hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function (req, res) {
