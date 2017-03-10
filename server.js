b30d732aa921c4e979a8aa944d827c200fa3c0fa
Ajitem <ajitempublic3@gmail.com>
2017-03-10 14:12:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.post('/create-user', function (req, res) {
     //JSON Request//
     var username= req.body.username;
     var password= req.body.password;
-    var salt = crypto.getRandomBytes(128).toString('hex');
+    var salt = crypto.RandomBytes(128).toString('hex');
     var dbString = hash(password,salt);
     pool.query('Insert into userhash (username,password) VALUES ($1,$2)',[username,dbString],function(err,res) {
               if (err) {
