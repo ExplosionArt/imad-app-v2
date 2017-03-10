0b64c4a252520762fa58a46fdb64d6bd00a227a6
Ajitem <ajitempublic3@gmail.com>
2017-03-10 11:59:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ function hash(input, salt) {
 }
 
 app.get('/hash/:input', function (req, res) {
-    var hashedString = hash(req.params.input);
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
 });
 
